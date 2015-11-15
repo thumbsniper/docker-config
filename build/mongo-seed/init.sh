7dc3d0b137f7375eae0bbce93f19a806087aad87
Thomas Schulte <thomas@cupracer.de>
2015-11-15 15:02:54 +0100

    add netcat to image check if MongoDB is reachable

--- build/mongo-seed/init.sh
+++ build/mongo-seed/init.sh
@@ -5,6 +5,11 @@ if test -z "$MONGOUSER" || test -z "$MONGOPASS" || test -z "$MONGODBNAME"; then
 	exit 1
 fi
 
+until nc -z thumbsniper-mongo.docker 27017
+do
+	echo "waiting for MongoDB"
+	sleep 1
+done
 
 mongo --host thumbsniper-mongo.docker --eval "
 	db = db.getSiblingDB(\"${MONGODBNAME}\");
